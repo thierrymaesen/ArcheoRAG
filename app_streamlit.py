e9c080a5b096714eaea343190c2da6bed3794aae
thierrymaesen <thierrymaesen@gmail.com>
2026-03-22 19:13:04 +0100

    Ajout du fix sqlite pour Streamlit Cloud

--- app_streamlit.py
+++ app_streamlit.py
@@ -1,3 +1,9 @@
+# --- FIX STREAMLIT CLOUD POUR CHROMADB ---
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+# -----------------------------------------
+
 import streamlit as st
 import os
 import tempfile
